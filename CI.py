b8ab0e45f1cbdc8bec38e786576c42cdc11dc69d
Robby Thomen <thomenr@health.missouri.edu>
2024-05-01 14:42:29 -0500

    fixed CI pxls save

--- CI.py
+++ CI.py
@@ -41,7 +41,7 @@ def getSpherePix(vox, radius):
     
     '''
     try:
-        pxls = np.load(os.path.join(os.getcwd(),f'c:/PIRL/python/spherePx/{radius}.npy'),pxls)
+        pxls = np.load(os.path.join(os.getcwd(),f'{radius}.npy'),pxls)
         print(f'\n spherePx of {radius} exists and is being loaded...')
     except:
         print(f"\n Building List of Pixels in sphere of radius {radius}...")
@@ -57,7 +57,7 @@ def getSpherePix(vox, radius):
             y = Y[circle]
             z = Z[circle]
             pxls = np.vstack((pxls,np.column_stack((np.repeat(r,len(x)),x,y,z))))
-        np.save(os.path.join(os.getcwd(),f'c:/PIRL/python/spherePx/{radius}.npy'),pxls)
+        np.save(os.path.join(os.getcwd(),f'{radius}.npy'),pxls)
         print(f"Sphere Pixel List of length {pxls.shape[0]} calculation time: {np.round(time.time()-starttime,2)} seconds")
     return pxls
 
